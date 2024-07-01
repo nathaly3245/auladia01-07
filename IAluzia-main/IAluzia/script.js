94b4763dc578749b51e3ce2c71848cb712371ecc
nathaly3245 <127852587+nathaly3245@users.noreply.github.com>
2024-07-01 11:03:31 -0300

    Add files via upload

---
+++ IAluzia-main/IAluzia/script.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const caixaResultado1 = document.querySelector(".caixa-principal");
+
+const perguntas = [
+    {
+        enunciado:"Todos os lugares do mundo tem acesso a cuidados para a saúde?",
+        alternativas:[
+            "não"
+            "pois em cidades pequenas normalmente tem menos acesso aos medicamentos e atendimentos necessários."
+        ]
+    },
+]
