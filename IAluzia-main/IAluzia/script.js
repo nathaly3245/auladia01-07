6ba1ebd3230c4567ffa2402fa0ca9e3e3fd18ad4
nathaly3245 <127852587+nathaly3245@users.noreply.github.com>
2024-07-29 09:01:35 -0300

    script.js

--- IAluzia-main/IAluzia/script.js
+++ IAluzia-main/IAluzia/script.js
@@ -10,8 +10,41 @@ const perguntas = [
     {
         enunciado:"Todos os lugares do mundo tem acesso a cuidados para a saúde?",
         alternativas:[
-            "não"
-            "pois em cidades pequenas normalmente tem menos acesso aos medicamentos e atendimentos necessários."
+           "pois em cidades pequenas normalmente tem menos acesso aos medicamentos e atendimentos necessários.",
+           "a tribo entende que a reprodução é necessario e nao deve ser contida.",
+            
         ]
     },
 ]
+const perguntas = [
+    {
+        enunciado:"Usar roupas enfeitadas na festa junina é necessário?",
+        alternativas:[
+            "Depende da festa e oque ela esta pedindo como traje.",
+            "a maioria das  festas é sim necessário ir com roupas enfeitadas",
+        ]
+    }
+]
+const perguntas = [
+    {
+        enunciado:"Todos os lixos recicláveis devem ser reciclados corretamente?",
+        alternativas:[
+            "Sim, para ter um meio ambiente mais limpo.",
+            "se for reciclável devemos sim reciclar para ajudar o lixeiros na coleta.",
+        ]
+    }
+];
+
+let atual = 0;
+let perguntaAtual;
+function mostraPerguntas(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntas [0].enunciado;
+}
+
+ function  mostraPerguntas(){
+    if(atual >=perguntas.length){
+        mostraResultado();
+        return;
+    }
+ }
