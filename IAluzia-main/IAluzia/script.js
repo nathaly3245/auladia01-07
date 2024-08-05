20c7aafc8851269327aede56f7f00ac641ef0977
nathaly3245 <127852587+nathaly3245@users.noreply.github.com>
2024-08-05 08:53:33 -0300

    script.js

--- IAluzia-main/IAluzia/script.js
+++ IAluzia-main/IAluzia/script.js
@@ -16,35 +16,60 @@ const perguntas = [
         ]
     },
 ]
-const perguntas = [
+
     {
         enunciado:"Usar roupas enfeitadas na festa junina é necessário?",
-        alternativas:[
+        alternativas;[
             "Depende da festa e oque ela esta pedindo como traje.",
             "a maioria das  festas é sim necessário ir com roupas enfeitadas",
         ]
     }
-]
-const perguntas = [
+
+
     {
         enunciado:"Todos os lixos recicláveis devem ser reciclados corretamente?",
-        alternativas:[
+        alternativas;[
             "Sim, para ter um meio ambiente mais limpo.",
             "se for reciclável devemos sim reciclar para ajudar o lixeiros na coleta.",
         ]
     }
-];
+
 
 let atual = 0;
 let perguntaAtual;
-function mostraPerguntas(){
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntas [0].enunciado;
-}
+let historiaFinal = "";
+
 
  function  mostraPerguntas(){
     if(atual >=perguntas.length){
         mostraResultado();
         return;
     }
+
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+ }
+
+ function mostraAlternativas(){
+    for(const alternativas of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativas.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+ }
+
+ function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+ }
+
+ function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
  }
