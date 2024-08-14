f65ac33c9e218450264b5475df638dbb6885ed23
nathaly3245 <127852587+nathaly3245@users.noreply.github.com>
2024-08-14 07:49:37 -0300

    script.js

--- IAluzia-main/IAluzia/script.js
+++ IAluzia-main/IAluzia/script.js
@@ -4,7 +4,6 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const caixaResultado1 = document.querySelector(".caixa-principal");
 
 const perguntas = [
     {
